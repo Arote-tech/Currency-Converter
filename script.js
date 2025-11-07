18649c3aef49e7c6d3a2ac023fdf037313bf4b7a
Rotimi Shaibu <shaiburotimi485@gmail.com>
2025-11-07 15:26:07 +0000

    installed vercel for deployment

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ async function fetchCurrencies () {
       // fetch("https://api.exchangerate-api.com/v4/latest/USD")
    const response = await fetch("https://api.exchangerate-api.com/v4/latest/USD");
    const data = await response.json();
+   
    const currencyOptions = Object.keys(data.rates);
 
    currencyOptions.forEach(currency => {
